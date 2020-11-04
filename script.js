7af1d3666143147ccefd86af07c595a7c4f8566b
Martina <martina.pozzerle@libero.it>
2020-11-04 17:30:25 +0100

    #implementazione dei dropdown menu

---
+++ script.js
@@ -0,0 +1,8 @@
+//quando passo sopra con il mouse do la classe active ai figli dell'elemento, e tolgo la classe a tutti gli altri elementi
+$(document).ready(function(){
+  $(".has-dropdown").hover(function(){
+    $(".has-dropdown").children().removeClass("active");
+    $(this).children().addClass("active");
+
+  })
+})
